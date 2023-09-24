62d13d88b0d60dc3846c518bf70f0270cd411f3f
libevm <95674753+libevm@users.noreply.github.com>
2023-09-24 23:22:53 +1000

    Update pubkey.py

--- pubkey.py
+++ pubkey.py
@@ -3,23 +3,31 @@ import argparse
 import eth_keys
 import hexbytes
 from eth_account._utils import signing
+from eth_account._utils.legacy_transactions import vrs_from
 
 
 def get_public_key(tx_raw):
     txn_bytes = hexbytes.HexBytes(tx_raw)
-    typed_txn = signing.TypedTransaction.from_bytes(txn_bytes)
 
-    msg_hash = typed_txn.hash()
+    try:
+        typed_txn = signing.TypedTransaction.from_bytes(txn_bytes)
+        msg_hash = typed_txn.hash()
+        vrs = typed_txn.vrs()
+
+    except:
+        txn = signing.Transaction.from_bytes(txn_bytes)
+        msg_hash = signing.hash_of_signed_transaction(txn)
+        vrs = vrs_from(txn)
+
     hash_bytes = hexbytes.HexBytes(msg_hash)
 
-    vrs = typed_txn.vrs()
     v, r, s = vrs
     v_standard = signing.to_standard_v(v)
     vrs = (v_standard, r, s)
 
     signature_obj = eth_keys.KeyAPI().Signature(vrs=vrs)
     pubkey = signature_obj.recover_public_key_from_msg_hash(hash_bytes)
-    
+
     return pubkey
 
 def parse_args():
@@ -29,6 +37,6 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    tx_raw = args.transaction    
+    tx_raw = args.transaction
     pubkey = get_public_key(tx_raw)
     print(f"Pubkey: {pubkey}")
